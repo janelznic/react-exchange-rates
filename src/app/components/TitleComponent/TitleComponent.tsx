26780cbd2a24326f1be5e16ce1f8d2507f26deea
Jan Elznic <jan@elznic.com>
2022-02-01 09:28:08 +0100

    Main title CSS position

--- src/app/components/TitleComponent/TitleComponent.tsx
+++ src/app/components/TitleComponent/TitleComponent.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const TitleComponent = styled.h1`
+  padding-left: 2em;
   padding-bottom: .5em;
   font-size: 2em;
-  text-align: center;
+  text-align: left;
   color: #000;
 `;
